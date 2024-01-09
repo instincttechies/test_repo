98f5affc81d362ac8c9955120d0c855157cd3da9
Ashok <menniyamdev@gmail.com>
2024-01-09 20:53:01 +0530

    Updated Deployment data

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-2paubq%a!h3u!zm---s#rcgb=yhxik=cu0d*x8)f8sb%im=(m+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['16.171.230.61']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "apitest2024.pythonanywhere.com"]
 
 
 # Application definition
@@ -85,10 +86,13 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 
