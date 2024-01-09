6a2531bbd5fe0d89fdb03995c7316973050b4d12
Ashok <menniyamdev@gmail.com>
2024-01-09 22:16:17 +0530

    Upload static files

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2paubq%a!h3u!zm---s#rcgb=yhxik=cu0d*x8)f8sb%im=(m+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "shankrishna.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "apitest2024.pythonanywhere.com"]
 
 
 # Application definition
@@ -127,11 +127,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
